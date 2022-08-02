c321f80a56620820f37ec8d3a44ebaca1dfde26c
artur <atqqq96@gmail.com>
2022-08-02 20:07:13 +0200

    deleted print / comments

--- computers/computers/spiders/computer_spider.py
+++ computers/computers/spiders/computer_spider.py
@@ -42,7 +42,6 @@ class ComputerSpider(scrapy.Spider):
                 './/div[contains(concat(" ",normalize-space(@class)," ")," pagination ")]//ul/li[last()-1]//a/text()'
             ).get()
         )
-        print(f"MAX PAGE NUMBER ************** {max_page_number}")
         for element in response.css("div.pe2-head"):
             url = element.css("a.blank-link::attr(href)").get()
             if url:
@@ -92,24 +91,3 @@ class ComputerSpider(scrapy.Spider):
                 )
 
         yield item_loader.load_item()
-
-        # with open('response.html', 'w') as html_file:
-        #     html_file.write(response.text)
-
-        # for computer in response.xpath('//div[contains(concat(" ",normalize-space(@class)," ")," row ")]//ul[contains(concat(" ",normalize-space(@class)," ")," product-entry2-wrap ")]//li[contains(concat(" ",normalize-space(@class)," ")," product-entry2 ")]'):
-        #     item_loader = ComputerItemLoader(ComputerItem(), computer)
-        #     item_loader.add_xpath('price', './/span[contains(concat(" ",normalize-space(@class)," ")," proper ")]/text()')
-        #     item_loader.add_xpath('name', './/div[contains(concat(" ",normalize-space(@class)," ")," pe2-head ")]//a/text()')
-        #     lst = computer.xpath('.//ul[contains(concat(" ",normalize-space(@class)," ")," key-features2 ")]/li//span[contains(concat(" ",normalize-space(@class)," ")," pe2-features__value ")]/text()').getall()
-        #     item_loader.add_xpath('chipset', './/div[contains(concat(" ",normalize-space(@class)," ")," inline-features ")]/text()')
-        #     item_loader.add_value('processor', lst[0] if lst else '')
-        #     item_loader.add_value('graphics', lst[1] if lst else '')
-        #     yield item_loader.load_item()
-        #
-        # self.STARTING_PAGE_NUMBER += 1
-        # if self.STARTING_PAGE_NUMBER > max_page_number:
-        #     self.STARTING_PAGE_NUMBER = 1
-        #     return
-        #
-        # next_page = f'https://www.komputronik.pl/search-filter/5801/komputery-do-gier?p={self.STARTING_PAGE_NUMBER}'
-        # yield scrapy.Request(url=next_page, callback=self.parse)
